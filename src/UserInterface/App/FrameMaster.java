20ccfc67e8423838c54775ea3e46a0ed38e3a744
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2023-09-04 21:56:19 -0500

    version paginada

--- src/UserInterface/App/FrameMaster.java
+++ src/UserInterface/App/FrameMaster.java
@@ -1,13 +1,19 @@
 package UserInterface.App;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import Framework.AppException;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
 public class FrameMaster extends JFrame {
     private PanelVideojuego panelVideojuego;
+    private JLabel lblTitulo;
 
     public FrameMaster(String titulo) throws AppException {
         personalizarFrame(titulo);
@@ -20,7 +26,7 @@ public class FrameMaster extends JFrame {
         setTitle(titulo);                           
         setLayout(new BorderLayout());
         setResizable(true);
-        setSize(1200, 720);
+        setSize(1000, 350);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
@@ -28,8 +34,16 @@ public class FrameMaster extends JFrame {
     }
 
     private void mostrarPanel() throws AppException {
+        JPanel panel = new JPanel();
+        Font fuente = new Font("Arial", Font.BOLD, 20);     
         panelVideojuego = new PanelVideojuego();
+        lblTitulo = new JLabel("LISTA DE VIDEOJUEGOS");
+        lblTitulo.setFont(fuente);
+        panel.add(lblTitulo);
+        panel.setPreferredSize(new Dimension(0, 50));
+        panel.setBackground(Color.WHITE);
         add(panelVideojuego, BorderLayout.CENTER);
+        add(panel, BorderLayout.NORTH);
         
     }
 }
--- src/UserInterface/App/PanelVideojuego.java
+++ src/UserInterface/App/PanelVideojuego.java
@@ -1,40 +1,68 @@
 package UserInterface.App;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
 import BusinessLogic.VideojuegoBL;
 import BusinessLogic.Entities.Videojuego;
 import Framework.AppException;
 
-public class PanelVideojuego extends JPanel {
-    private VideojuegoBL videojuegoBL;
-    private Videojuego videojuego;
+public class PanelVideojuego extends JPanel implements ActionListener{
+    private VideojuegoBL 
+            videojuegoBL;
+    private JTable 
+            tabla;
+    private JButton 
+            btnPrev,
+            btnNext,
+            btnInicio,
+            btnFinal;
+    private JPanel
+            pnlBoton;
+    private DefaultTableModel 
+            tableModel;
+    private int 
+            pagActual,
+            cantFilas,
+            pagMax;
+    private String[] 
+            header;
+    private Object[][] 
+            data;
+    private JLabel
+            lblPagina;
 
     PanelVideojuego() throws AppException {
         inicializarRecursos();
-        mostrarTabla();
+        activarBotones();
         personalizarPanel();
+        mostrarTabla();
         setVisible(true);
     }
 
-    private void inicializarRecursos() {
-        videojuego   = new Videojuego();
+    private void inicializarRecursos() throws AppException {
         videojuegoBL = new VideojuegoBL();
-    }
+        btnPrev     = new JButton(" << ");
+        btnNext     = new JButton(" >> ");
+        btnInicio   = new JButton(" |< ");
+        btnFinal    = new JButton(" >| ");
+        lblPagina   = new JLabel();
+        pnlBoton    = new JPanel();
 
-    private void personalizarPanel() {
-        setLayout(new FlowLayout(FlowLayout.CENTER, 10, 50));
-    }
 
-    private void mostrarTabla() throws AppException {
-        String[] header = {"Id", "Titulo", "Distribuidor", "Plataforma Popular", "Anio Lanzamiento", "Ventas (millones)"};
-        Object[][] data = new Object[videojuegoBL.getRegistrosActivos().size()][6];  
+        pagActual = 0;
+        cantFilas = 5;
+
+        header = new String[]{"Id", "Titulo", "Distribuidor", "Plataforma Popular", "Anio Lanzamiento", "Ventas (millones)"};
+        data = new Object[videojuegoBL.getRegistrosActivos().size()][6];  
         int index = 0;
         for(Videojuego v : videojuegoBL.getRegistrosActivos()) {
             data[index][0] = v.getId();
@@ -45,11 +73,68 @@ public class PanelVideojuego extends JPanel {
             data[index][5] = v.getVentasEstimadas() / 1000000;
             index++;
         }
+
+        pagMax = (data.length - 1) / cantFilas;
+    }
+
+    private void activarBotones() {
+        btnNext.addActionListener(this);
+        btnPrev.addActionListener(this);
+        btnInicio.addActionListener(this);
+        btnFinal.addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnNext) 
+            if (pagActual < pagMax)
+                pagActual++;
+        if (e.getSource() == btnPrev)
+            if (pagActual > 0) 
+                pagActual--;
+        if (e.getSource() == btnInicio) 
+            pagActual = 0;
+        if (e.getSource() == btnFinal)
+            pagActual = pagMax;
+        actualizarTabla();
+    }
+
+    
+
+    private void actualizarTabla() {
+        tableModel.setRowCount(0); 
+        int start = pagActual * cantFilas;
+        int end = start + cantFilas;
         
-        JTable tabla  = new JTable(data, header);
-        int rowHeight = 30; // Altura deseada en píxeles
-        Font fuente = new Font("Arial", Font.PLAIN, 16);               // Tipo y tamaño de letra deseado
-        Font fuente2 = new Font("Arial", Font.BOLD, 16);               // Tipo y tamaño de letra deseado
+        for (int i = start; i < end; i++) {
+            Object[] fila = data[i];
+            tableModel.addRow(fila);
+        }
+
+        actualizarIndicadorPagina();
+    }
+
+    private void actualizarIndicadorPagina() {
+        String textoPagina  = " Page  :  [  "
+                            + (pagActual + 1)
+                            + "  of  "
+                            + (pagMax + 1)
+                            + "  ]  ";
+        lblPagina.setText(textoPagina);
+    }
+
+    private void personalizarPanel() {
+        setLayout(new BorderLayout());
+    }
+
+    private void mostrarTabla() {
+        int rowHeight = 30;
+        Font fuente = new Font("Arial", Font.PLAIN, 16);               
+        Font fuente2 = new Font("Arial", Font.BOLD, 16);               
+        
+        tableModel = new DefaultTableModel(data, header);
+
+        tabla = new JTable(tableModel);
         tabla.setShowHorizontalLines(true);
         tabla.setGridColor(Color.LIGHT_GRAY);
         tabla.setRowSelectionAllowed(true);
@@ -60,9 +145,20 @@ public class PanelVideojuego extends JPanel {
         tabla.setRowHeight(rowHeight);
         tabla.getTableHeader().setFont(fuente2); // Configura el renderizador de encabezado personalizado
         tabla.setFont(fuente);
+        
+        agregarComponentes();
+        actualizarTabla();
 
-        JScrollPane scrollPane = new JScrollPane(tabla);
-        add(scrollPane);
+    }
+
+    private void agregarComponentes() {
+        pnlBoton.add(btnInicio);
+        pnlBoton.add(btnPrev);
+        pnlBoton.add(lblPagina);
+        pnlBoton.add(btnNext);
+        pnlBoton.add(btnFinal);
+        add(new JScrollPane(tabla), BorderLayout.CENTER);
+        add(pnlBoton, BorderLayout.SOUTH);
     }
 
     
